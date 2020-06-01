0427b5209d5f0decad605035fb5aac8f7ad01ced
Duong <duongtuanphuong2@gmail.com>
2020-06-02 00:20:24 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -54,31 +54,6 @@ jQuery(document).ready(function ($) {
         autoplay: true,
         autoplayspeed: 2000,
         prevArrow:".left",
-        nextArrow:'.right',
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 4,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 720,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-
-        ]
+        nextArrow:'.right'
     });
 });
